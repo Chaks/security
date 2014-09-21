a3b62870c46ce17f3e774e8e7a80b577d5335147
Chaks <darimireddy@googlemail.com>
2014-09-22 01:25:12 +0530

    added XSS filter based on OWASP ESAPI

---
+++ xss_filter/src/main/java/com/mycompany/web/filter/CustomSecurityWrapperRequest.java
@@ -0,0 +1,89 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.web.filter;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import javax.servlet.ServletInputStream;
+import javax.servlet.http.HttpServletRequest;
+import org.apache.commons.io.IOUtils;
+import org.owasp.esapi.ESAPI;
+import org.owasp.esapi.errors.IntrusionException;
+import org.owasp.esapi.errors.ValidationException;
+import org.owasp.esapi.filters.SecurityWrapperRequest;
+
+/**
+ *
+ * Credits to
+ * http://stackoverflow.com/questions/10210645/http-servlet-request-lose-params-from-post-body-after-read-it-once
+ * for MultiReadHttpServletRequest
+ *
+ * @author Chakravarthi
+ */
+public class CustomSecurityWrapperRequest extends SecurityWrapperRequest {
+
+  private ByteArrayOutputStream cachedBytes;
+
+  public CustomSecurityWrapperRequest(HttpServletRequest request) {
+    super(request);
+  }
+
+  @Override
+  public String getParameter(String name, boolean allowNull, int maxLength, String regexName) {
+    String orig = getHttpServletRequest().getParameter(name);
+    String clean = null;
+    try {
+      clean = ESAPI.validator().getValidInput("HTTP parameter name: " + name, orig, regexName, maxLength, allowNull);
+    } catch (ValidationException e) {
+      throw new IntrusionException("Intrusion detected", "Throwing sorry/error page, " + e.getMessage());
+    }
+    return clean;
+  }
+
+  @Override
+  public ServletInputStream getInputStream() throws IOException {
+    if (cachedBytes == null) {
+      cacheInputStream();
+    }
+    return new CachedServletInputStream();
+  }
+
+  @Override
+  public BufferedReader getReader() throws IOException {
+    return new BufferedReader(new InputStreamReader(getInputStream()));
+  }
+
+  private HttpServletRequest getHttpServletRequest() {
+    return (HttpServletRequest) super.getRequest();
+  }
+
+  private void cacheInputStream() throws IOException {
+    /* Cache the inputstream in order to read it multiple times. For 
+     * convenience, I use apache.commons IOUtils
+     */
+    cachedBytes = new ByteArrayOutputStream();
+    IOUtils.copy(super.getInputStream(), cachedBytes);
+  }
+
+  /* An inputstream which reads the cached request body */
+  public class CachedServletInputStream extends ServletInputStream {
+
+    private ByteArrayInputStream input;
+
+    public CachedServletInputStream() {
+      /* create a new input stream from the cached request body */
+      input = new ByteArrayInputStream(cachedBytes.toByteArray());
+    }
+
+    @Override
+    public int read() throws IOException {
+      return input.read();
+    }
+  }
+}
---
+++ xss_filter/src/main/java/com/mycompany/web/filter/SecureRequestFilter.java
@@ -0,0 +1,168 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.web.filter;
+
+import java.io.IOException;
+import java.io.InputStream;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.http.HttpServletRequest;
+import org.apache.commons.io.IOUtils;
+import org.owasp.esapi.errors.IntrusionException;
+
+/**
+ *
+ * @author Chakravarthi
+ */
+@WebFilter(filterName = "SecureRequestFilter", urlPatterns = {"/*"},
+        initParams = @WebInitParam(name = "intrusionErrorPage", value = "/WEB-INF/intrusionError.jsp"))
+public class SecureRequestFilter implements Filter {
+
+  private static final boolean debug = true;
+  private FilterConfig filterConfig = null;
+
+  public SecureRequestFilter() {
+  }
+
+  public void doBeforeProcessing(ServletRequest request, ServletResponse response)
+          throws IOException, ServletException {
+    if (debug) {
+      log("SecureRequestFilter:DoBeforeProcessing");
+    }
+  }
+
+  public void doAfterProcessing(ServletRequest request, ServletResponse response)
+          throws IOException, ServletException {
+    if (debug) {
+      log("SecureRequestFilter:DoAfterProcessing");
+    }
+  }
+
+  /**
+   *
+   * @param request The servlet request we are processing
+   * @param response The servlet response we are creating
+   * @param chain The filter chain we are processing
+   *
+   * @exception IOException if an input/output error occurs
+   * @exception ServletException if a servlet error occurs
+   */
+  @Override
+  public void doFilter(ServletRequest request, ServletResponse response,
+          FilterChain chain)
+          throws IOException, ServletException {
+
+    if (debug) {
+      log("SecureRequestFilter:doFilter()");
+    }
+
+    doBeforeProcessing(request, response);
+
+    HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+    //System.out.println("getContextPath=> " + httpServletRequest.getRequestURI());
+
+    request.setAttribute("validationDone", "true");
+    CustomSecurityWrapperRequest customSecurityWrapperRequest = new CustomSecurityWrapperRequest(httpServletRequest);
+    customSecurityWrapperRequest.setAllowableContentRoot("WEB-INF");
+
+    //Handle for POST JSON data
+    if ("POST".equalsIgnoreCase(customSecurityWrapperRequest.getMethod())) {
+      String postData = IOUtils.toString((InputStream) customSecurityWrapperRequest.getInputStream(), "UTF-8");
+      log("[postData] " + postData);
+      //TODO:
+    }
+    
+    Throwable problem = null;
+    try {
+      chain.doFilter(customSecurityWrapperRequest, response);
+    } catch (IOException ioe) {
+      problem = ioe;
+    } catch (ServletException se) {
+      problem = se;
+    } catch (IntrusionException ie) {
+      log("Intrusion detected, throwing sorry/error page");
+      problem = ie;
+    }
+
+    doAfterProcessing(customSecurityWrapperRequest, response);
+
+    if (problem != null) {
+      if (problem instanceof ServletException) {
+        throw (ServletException) problem;
+      }
+      if (problem instanceof IOException) {
+        throw (IOException) problem;
+      }
+      RequestDispatcher requestDispatcher = customSecurityWrapperRequest.getRequestDispatcher(filterConfig.getInitParameter("intrusionErrorPage"));
+      requestDispatcher.forward(customSecurityWrapperRequest, response);
+      //sendProcessingError(problem, response);
+    }
+  }
+
+  /**
+   * Return the filter configuration object for this filter.
+   *
+   * @return
+   */
+  public FilterConfig getFilterConfig() {
+    return (this.filterConfig);
+  }
+
+  /**
+   * Set the filter configuration object for this filter.
+   *
+   * @param filterConfig The filter configuration object
+   */
+  public void setFilterConfig(FilterConfig filterConfig) {
+    this.filterConfig = filterConfig;
+  }
+
+  /**
+   * Destroy method for this filter
+   */
+  @Override
+  public void destroy() {
+  }
+
+  /**
+   * Init method for this filter
+   *
+   * @param filterConfig
+   */
+  @Override
+  public void init(FilterConfig filterConfig) {
+    this.filterConfig = filterConfig;
+    if (filterConfig != null) {
+      if (debug) {
+        log("SecureRequestFilter:Initializing filter");
+      }
+    }
+  }
+
+  /**
+   * Return a String representation of this object.
+   */
+  @Override
+  public String toString() {
+    if (filterConfig == null) {
+      return ("SecureRequestFilter()");
+    }
+    StringBuilder sb = new StringBuilder("SecureRequestFilter(");
+    sb.append(filterConfig);
+    sb.append(")");
+    return (sb.toString());
+  }
+
+  public void log(String msg) {
+    filterConfig.getServletContext().log(msg);
+  }
+}
